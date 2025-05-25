fb3e43045fe0f12fd111b5673a3c1629f3a539f7
Keijiro_Ida <idatt1122@gmail.com>
2025-05-25 17:11:26 +0900

    first commit

---
+++ src/lib/prisma.js
@@ -0,0 +1,9 @@
+import { PrismaClient } from '../generated/prisma';
+
+const prisma = global.prisma ?? new PrismaClient({log: ['query']});
+
+if(process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
+
+export default prisma;
